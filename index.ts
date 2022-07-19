2a7a7a244ba3fa09f8775b755381fad3b9ed9bff
Chris Gemmell <fablekey@gmail.com>
2022-07-19 20:21:47 +1000

    changed to typescript for easier development

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { cursorTo, moveCursor } from "node:readline";
+import path from "node:path";
+import { createWriteStream } from 'fs';
+import ytdl from 'ytdl-core';
+import { askForVideo, askFormats, grabVidDetails, selectFormat } from './lib.js';
+import { fileURLToPath } from "node:url";
+
+const filePath = fileURLToPath(import.meta.url)
+const dirName = path.dirname(filePath)
+
+const answer = await askForVideo()
+const formatAnswer = await askFormats()
+const video = await grabVidDetails(answer)
+const formats = ytdl.filterFormats(video.formats, formatAnswer)
+const selectedFormat = await selectFormat(formats)
+
+downloadVideo(video.videoDetails, selectedFormat)
+
+function downloadVideo(vidDeets: ytdl.MoreVideoDetails, qual: string) {
+    const vUrl = vidDeets.video_url
+    const vName = vidDeets.title
+
+    const output = path.resolve(dirName, `videos/${vName}.mp4`);
+
+    const video = ytdl(vUrl, {
+        quality: qual
+    });
+
+    let starttime: number
+    video.pipe(createWriteStream(output));
+    video.once('response', () => {
+        starttime = Date.now();
+    });
+    video.on('progress', (chunkLength, downloaded, total) => {
+        const percent = downloaded / total;
+        const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+        const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
+        cursorTo(process.stdout, 0);
+        process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+        process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
+        process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
+        process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
+        moveCursor(process.stdout, 0, -1);
+    });
+    video.on('end', () => {
+        process.stdout.write('\n\n');
+        process.exit(1)
+    });
+}
